352a23d50cda26a9079f0613672c9f0d2f224b49
Jani Anttila <jva@1BQQDNP.viikki.dhcp.hupnet.helsinki.fi>
2017-05-02 16:32:09 +0300

    minor additions

--- src/fn2.cpp
+++ src/fn2.cpp
@@ -7,15 +7,15 @@
 typedef boost::multi_array<double,2> matriisi;
 typedef matriisi::index indeksi;
 
-// a function for a single fnoise run
+// a function for a dual fnoise run
 // mean 0.0 sd 1.0
 RcppExport SEXP fn2(SEXP time, SEXP gamma, SEXP korrelaatio)
 {
   std::random_device rd;
   std::mt19937 mt(rd());
-  std::uniform_real_distribution<double> dist(1.0, 10.0);
+  std::uniform_real_distribution<double> dist(0.0, 1.0);
   const double pii = 3.14159265359;
-  double t, g, roo, ss, spec, vaihe, freq;
+  double t, g, roo, ss, spec, vaihe1, vaihe2, freq;
   Rcpp::NumericVector timev(time);
   Rcpp::NumericVector gammav(gamma);
   Rcpp::NumericVector roov(korrelaatio);
@@ -23,15 +23,19 @@ RcppExport SEXP fn2(SEXP time, SEXP gamma, SEXP korrelaatio)
   g = gammav[0];
   roo = roov[0];
   ss = t/2;
-  std::vector<double> w, x;
-  w.resize(t);
-  x.resize(t);
-  for(int i = 0; i < t; ++i){
-    w[i] = 0.0;
-    x[i] = i*(2.0*pii/((double)t - 1.0));
+  matriisi::extent_get extents;
+  matriisi w, x;
+  w.resize(extents[2][t]);
+  x.resize(extents[2][t]);
+  for(int i = 0; i < 2; ++i){
+    for(int j = 0; j < t; ++j){
+      w[i][j] = 0.0;
+      x[i][j] = j*(2.0*pii/((double)t - 1.0));
+    }
   }
   for(int i = 0; i < ss; ++i){
-    vaihe = dist(mt) * 2.0 * pii;
+    vaihe1 = dist(mt) * 2.0 * pii;
+    vaihe2 = acos(roo) + vaihe2;
     freq = 1.0 + (double)i;
     spec = 1.0 / (pow(freq,g));
     for(int k = 1; k < t; ++k){
@@ -52,5 +56,7 @@ RcppExport SEXP fn2(SEXP time, SEXP gamma, SEXP korrelaatio)
   for(int i = 0; i < t; ++i){
     ulos[i] = (w[i]-ka)/khaj;
   }
+  w.resize(extents[0][0]);
+  x.resize(extents[0][0]);
   return(ulos);
 }
