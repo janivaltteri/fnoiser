21e109de839cba1309a670647647e7fca0377514
Jani Anttila <jva@1BQQDNP.viikki.dhcp.hupnet.helsinki.fi>
2017-05-02 15:52:19 +0300

    fn2 started

--- R/fnoise.R
+++ R/fnoise.R
@@ -2,3 +2,8 @@ fn1 <- function(time,gamma){
     require(Rcpp)
     .Call('fn1',c(time),c(gamma),PACKAGE='fnoiser')
 }
+
+fn2 <- function(time,gamma,correlation){
+    require(Rcpp)
+    .Call('fn2',c(time),c(gamma),c(correlation),PACKAGE='fnoiser')
+}
