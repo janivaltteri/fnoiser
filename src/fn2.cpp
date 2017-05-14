de76a4dac27c0e9f7f882f113e081fc765a1d65a
Jani V Anttila <jani.v.anttila@gmail.com>
2017-05-14 13:22:40 +0300

    fn2 does not segfault

--- src/fn2.cpp
+++ src/fn2.cpp
@@ -2,10 +2,6 @@
 #include <random>
 #include <vector>
 #include <math.h>
-#include "boost/multi_array.hpp"
-
-typedef boost::multi_array<double,2> matriisi;
-typedef matriisi::index indeksi;
 
 // a function for a dual fnoise run
 // mean 0.0 sd 1.0
@@ -15,7 +11,9 @@ RcppExport SEXP fn2(SEXP time, SEXP gamma, SEXP korrelaatio)
   std::mt19937 mt(rd());
   std::uniform_real_distribution<double> dist(0.0, 1.0);
   const double pii = 3.14159265359;
-  double t, g, roo, ss, spec, vaihe1, vaihe2, freq;
+  int t, ss;
+  double g, roo, spec, vaihe1, vaihe2, freq;
+  double ka1, ka2, khaj1, khaj2;
   Rcpp::NumericVector timev(time);
   Rcpp::NumericVector gammav(gamma);
   Rcpp::NumericVector roov(korrelaatio);
@@ -23,10 +21,12 @@ RcppExport SEXP fn2(SEXP time, SEXP gamma, SEXP korrelaatio)
   g = gammav[0];
   roo = roov[0];
   ss = t/2;
-  matriisi::extent_get extents;
-  matriisi w, x;
-  w.resize(extents[2][t]);
-  x.resize(extents[2][t]);
+  double** w = new double*[2];
+  double** x = new double*[2];
+  for(int i = 0; i < 2; ++i){
+    w[i] = new double[t];
+    x[i] = new double[t];
+  }
   for(int i = 0; i < 2; ++i){
     for(int j = 0; j < t; ++j){
       w[i][j] = 0.0;
@@ -35,28 +35,40 @@ RcppExport SEXP fn2(SEXP time, SEXP gamma, SEXP korrelaatio)
   }
   for(int i = 0; i < ss; ++i){
     vaihe1 = dist(mt) * 2.0 * pii;
-    vaihe2 = acos(roo) + vaihe2;
+    vaihe2 = acos(roo) + vaihe1;
     freq = 1.0 + (double)i;
     spec = 1.0 / (pow(freq,g));
     for(int k = 1; k < t; ++k){
-      w[k] = w[k] + spec*sin(x[k]*freq-vaihe);
+      w[0][k] = w[0][k] + spec*sin(x[0][k]*freq-vaihe1);
+      w[1][k] = w[1][k] + spec*sin(x[1][k]*freq-vaihe2);
     }
   }
-  double ka = 0.0;
-  double khaj = 0.0;
+  ka1 = 0.0;
+  khaj1 = 0.0;
+  ka2 = 0.0;
+  khaj2 = 0.0;
   for(int i = 0; i < t; ++i){
-    ka += w[i];
+    ka1 += w[0][i];
+    ka2 += w[1][i];
   }
-  ka = ka / t;
+  ka1 = ka1 / t;
+  ka2 = ka2 / t;
   for(int i = 0; i < t; ++i){
-    khaj += (w[i]-ka)*(w[i]-ka);
+    khaj1 += (w[0][i]-ka1)*(w[0][i]-ka1);
+    khaj2 += (w[1][i]-ka2)*(w[1][i]-ka2);
   }
-  khaj = sqrt(khaj/((double)t - 1.0));
+  khaj1 = sqrt(khaj1/((double)t - 1.0));
+  khaj2 = sqrt(khaj2/((double)t - 1.0));
   Rcpp::NumericMatrix ulos(t,2);
   for(int i = 0; i < t; ++i){
-    ulos[i] = (w[i]-ka)/khaj;
+    ulos(i,0) = (w[0][i]-ka1)/khaj1;
+    ulos(i,1) = (w[1][i]-ka2)/khaj2;
   }
-  w.resize(extents[0][0]);
-  x.resize(extents[0][0]);
+  for(int i = 0; i < 2; ++i){
+    delete [] w[i];
+    delete [] x[i];
+  }
+  delete [] w;
+  delete [] x;
   return(ulos);
 }
